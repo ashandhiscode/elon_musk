a4de660443ca843a2c2bd9439560e548de6d8ecb
ashandhiscode <73539201+ashandhiscode@users.noreply.github.com>
2020-10-30 16:17:57 +0000

    Add files via upload

--- elon_musk.py
+++ elon_musk.py
@@ -1,1 +1,73 @@
-print("Elon Musk.")
+import sys
+sys.path.append('./')
+from email_bot import EmailBot
+import time
+import random
+
+class ElonMusk() :
+    player_details = {}
+    world_issue = ""
+
+    def __init__(self, number_of_players) :
+        if not isinstance(number_of_players, int) :
+            raise Exception(f"{number_of_players} must be int.")
+        else :
+            self.number_of_players = number_of_players
+            self.play()
+    
+    def get_player_info(self) :
+        verified = False
+        while not verified :
+            name = input("Name: ")
+            email = input("Email address: ")
+            verification_response = input(f"\nPlease confirm that the following details are correct:\nName: {name}. Email address: {email}\nY/n?")
+            if verification_response.lower().replace(' ', '') != 'y' :
+                print("Please reenter your details:")
+                continue
+            else :
+                print("Confirmed")
+                self.player_details[name.lower().replace(' ', '')] = email.lower().replace(' ', '')
+                verified=True
+        print("")
+
+    def get_players_info(self) :
+        for i in range(self.number_of_players) :
+            print(f"Player {i+1} please insert your details.")
+            self.get_player_info()
+
+    def print_players_summary(self) :
+        i=1
+        for name, email in self.player_details.items() :
+            print(f"Player {i}: {name}, {email}")
+            i+=1
+
+    def print_rules(self) :
+        print("Welcome to your game of Elon Musk.\n")
+
+    def get_world_issue(self) :
+        self.world_issue = "Climate change"
+
+    def launch_emailbot(self) :
+        self.emailbot = EmailBot(self.world_issue, self.elon)
+
+    def send_email(self, email, name) :
+        self.emailbot.send_email(email, name)
+
+    def select_elon(self) :
+        self.elon = random.choice(list(self.player_details.keys()))
+
+    def send_emails(self) :
+        self.launch_emailbot()
+        for name, email in self.player_details.items() :
+            self.send_email(email, name)
+
+    def play(self) :
+        self.print_rules()
+        time.sleep(5)
+        self.get_players_info()
+        self.print_players_summary()
+        self.get_world_issue()
+        self.select_elon()
+        self.send_emails()
+
+ElonMusk(3)
---
+++ email_bot.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('./')
+import config
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+class EmailBot() :
+    email_address = r"elonmuskrobot@gmail.com"
+    auth = "yvijqjwetxfxltbr"
+
+    def __init__(self, world_issue, elon) :
+        self.world_issue = world_issue
+        self.elon = elon
+
+    def return_text(self, elonmusk=False) :
+        if elonmusk :
+            mail_content = '''You are Elon Musk.'''
+        else :
+            mail_content = f'''\n\n\n\nThe world issue is: {self.world_issue}'''
+        return(mail_content)
+
+    def generate_MIME(self, recipient, name) :
+        self.message = MIMEMultipart()
+        self.message['From'] = self.email_address
+        self.message['Subject'] = "Open this email to find out which world issue you're going to be facing..."
+        self.message['To'] = recipient
+        self.message.attach(MIMEText(self.return_text(name==self.elon), 'plain'))
+    
+    def launch_smpt_session(self) :
+        self.session = smtplib.SMTP('smtp.gmail.com', 587)
+        self.session.starttls()
+        self.session.login(self.email_address, self.auth)
+
+    def send_email(self, recipient, name) :
+        self.generate_MIME(recipient, name)
+        self.launch_smpt_session()
+        text = self.message.as_string()
+        self.session.sendmail(self.email_address, recipient, text)
+        self.session.quit()
